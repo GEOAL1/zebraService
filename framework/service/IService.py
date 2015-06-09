0818e891f6eb408e9ebdbffe32bda006c4d9c362
Administrator <Administrator@PC201401211223>
2015-06-09 23:34:50 +0800

    操作账户的dao，

--- framework/service/IService.py
+++ framework/service/IService.py
@@ -1,8 +1,11 @@
 from subsystem.SM.server.dao.userDao import UserDao
+from subsystem.AM.server.dao.accountDao import AccountDao
 
 
 class IService(object):
     def __init__(self):
         self.userDao = UserDao()
+        #待测试
+#         self.accountDao = AccountDao()
         pass
 pass
